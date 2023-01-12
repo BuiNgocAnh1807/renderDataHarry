33611a4328528076c0b67d6ced84e61ef4f91baa
BuiNgocAnh1807 <ngocanhptit1807@gmail.com>
2023-01-12 16:38:14 +0700

    change promiseData

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ function clickCallApi() {
     const dataList = data.slice(0, 10);
     return dataList;
   }
-
   axios.interceptors.request.use(
     function (config) {
       getIdLoading.classList.add("load");
@@ -17,7 +16,9 @@ function clickCallApi() {
       return Promise.reject(error);
     }
   );
-
+  const promiseData = new Promise((resolve, reject) => {
+    resolve(getData());
+  });
   axios.interceptors.response.use(
     function (response) {
       getIdLoading.classList.remove("load");
@@ -27,81 +28,82 @@ function clickCallApi() {
       return Promise.reject(error);
     }
   );
-  async function render() {
-    const dataRender = await getData();
-    let lists = "";
-    dataRender.map((data, index) => {
-      lists += `<div class="card__list-group" data-bs-toggle="modal" data-bs-target="#list${index}">
-            <img src="${data.image}" alt="Avatar" style="width: 200px ; height: 200px; margin-top: 10px; padding: 10px 20px;">
-            <div class="card__description">
-              <h4><b style="margin: 0 5px;">${data.name}</b></h4> 
-              <div class="card--container">
-              <ul class="card__description--list">
-                <li class='card__description-text'>${data.gender}</li> 
-                <li class='card__description-text'>${data.ancestry}</li> 
-                <li class='card__description-text'>${data.eyeColour}</li> 
-                <li class='card__description-text'>${data.patronus}</li> 
-             </ul>
+  function render() {
+    promiseData.then((res) => {
+      let lists = "";
+      res.map((data, index) => {
+        lists += `<div class="card__list-group" data-bs-toggle="modal" data-bs-target="#list${index}">
+              <img src="${data.image}" alt="Avatar" style="width: 200px ; height: 200px; margin-top: 10px; padding: 10px 20px;">
+              <div class="card__description">
+                <h4><b style="margin: 0 5px;">${data.name}</b></h4> 
+                <div class="card--container">
+                <ul class="card__description--list">
+                  <li class='card__description-text'>${data.gender}</li> 
+                  <li class='card__description-text'>${data.ancestry}</li> 
+                  <li class='card__description-text'>${data.eyeColour}</li> 
+                  <li class='card__description-text'>${data.patronus}</li> 
+               </ul>
+                </div>
               </div>
+              </div>
+    
+            <div class="modal fade" id="list${index}" tabindex="-1" aria-labelledby="list${index}Label" aria-hidden="true">
+              <div class="modal-dialog modal-dialog-centered">
+              <div class="modal-content">
+              <div class="modal-header">
+              <h5 class="modal-title">${data.name}</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+            <div class="modal-body__container">
+            <img src="${data.image}" alt="Avatar" style="width: 200px ; height: 200px;" class="img-responsive" />
+    
+            <table border="0" cellpadding="2">
+            <tr>
+                <th>Gender</th>
+                <td>${data.gender}</td>
+            </tr>
+            <tr>
+                <th>Ancestry</th>
+                <td>${data.ancestry}</td>
+            </tr>
+            <tr>
+                <th>EyeColour</th>
+                <td>${data.eyeColour}</td>
+            </tr>
+            <tr>
+                <th>Patronus</th>
+                <td>${data.patronus}</td>
+            </tr>
+            <tr>
+                <th>HairColour</th>
+                <td>${data.hairColour}</td>
+            </tr>
+            <tr>
+                <th>Alive</th>
+                <td>${data.alive}</td>
+            </tr>
+            <tr>
+                <th>Wizard</th>
+                <td>${data.wizard}</td>
+            </tr>
+            <tr>
+                <th>YearOfBirth</th>
+                <td>${data.yearOfBirth}</td>
+            </tr>
+        </table>
             </div>
             </div>
-  
-          <div class="modal fade" id="list${index}" tabindex="-1" aria-labelledby="list${index}Label" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered">
-            <div class="modal-content">
-            <div class="modal-header">
-            <h5 class="modal-title">${data.name}</h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-          <div class="modal-body__container">
-          <img src="${data.image}" alt="Avatar" style="width: 200px ; height: 200px;" class="img-responsive" />
-  
-          <table border="0" cellpadding="2">
-          <tr>
-              <th>Gender</th>
-              <td>${data.gender}</td>
-          </tr>
-          <tr>
-              <th>Ancestry</th>
-              <td>${data.ancestry}</td>
-          </tr>
-          <tr>
-              <th>EyeColour</th>
-              <td>${data.eyeColour}</td>
-          </tr>
-          <tr>
-              <th>Patronus</th>
-              <td>${data.patronus}</td>
-          </tr>
-          <tr>
-              <th>HairColour</th>
-              <td>${data.hairColour}</td>
-          </tr>
-          <tr>
-              <th>Alive</th>
-              <td>${data.alive}</td>
-          </tr>
-          <tr>
-              <th>Wizard</th>
-              <td>${data.wizard}</td>
-          </tr>
-          <tr>
-              <th>YearOfBirth</th>
-              <td>${data.yearOfBirth}</td>
-          </tr>
-      </table>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
           </div>
           </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
           </div>
-        </div>
-        </div>
-        </div>
-            `;
+              `;
+      });
+      document.querySelector(".card").innerHTML = lists;
     });
-    document.querySelector(".card").innerHTML = lists;
   }
   render();
   const elementBtn = document.querySelector(".btn-api");
